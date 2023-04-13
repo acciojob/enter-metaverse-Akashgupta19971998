7a99d0fe291fd00082a1957e93aaa8327064183d
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-04-13 18:18:46 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+console.log("hi")
+let head=document.getElementById("heading").style.display="none";
+var op=document.getElementById("status")
+function clicked(){
+     console.log("hello");
+    // op.innerHTML="<h1>Enter the Metaverse</h1>"
+    // head.style.display="block";
+     op.style.display="none";
+     let head=document.getElementById("heading").style.display="block";
+}
